9d1faf66ac018438498bf43a1f92531502ad87a7
hakuunabatata <emachadomartins1999@gmail.com>
2024-04-16 15:04:20 -0300

    chore: document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export const Document: React.FC = () => (
-  <main className="flex-1 flex items-center justify-center text-rotion-400">
-    Document
-    <Link to="/">Acessar blank</Link>
+  <main className="flex-1 flex py-12 px-10 gap-8">
+    <aside className="hidden lg:block sticky top-0">
+      <span className="text-rotion-300 font-semibold uppercase  text-xs ">Table of Contents</span>
+
+      <ToC.Root>
+        <ToC.Link>Back-End</ToC.Link>
+        <ToC.Section>
+          <ToC.Link>DB</ToC.Link>
+          <ToC.Link>Auth</ToC.Link>
+        </ToC.Section>
+        <ToC.Link>Front-End</ToC.Link>
+        <ToC.Section>
+          <ToC.Link>Visualization</ToC.Link>
+          <ToC.Link>Styles</ToC.Link>
+        </ToC.Section>
+      </ToC.Root>
+    </aside>
+
+    <section className="flex-1 flex flex-col items-center">hjdiuhwaqeufdh</section>
   </main>
 )
